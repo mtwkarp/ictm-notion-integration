3a29e680679e12c727b1a767d1e57c5233e036d0
Matvii Karpenko <42414976+mtwkarp@users.noreply.github.com>
2024-09-18 15:53:23 +0300

    Removed dependencies which blocks deploy on Heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const express = require("express");
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/dist/index.html')
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlInlineCssWebpackPlugin = require('html-inline-css-webpack-plugin').default;
-const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = {
     entry: './instructorScheduleButtonPage/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        clean: true
     },
     module: {
         rules: [
@@ -22,11 +22,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './instructorScheduleButtonPage/index.html',
-            inject: 'body', // Inject all assets into the body
         }),
         new HtmlInlineCssWebpackPlugin(),
-        new ScriptExtHtmlWebpackPlugin({
-            inline: 'bundle.js', // Inline the JavaScript bundle
-        }),
     ],
 };
